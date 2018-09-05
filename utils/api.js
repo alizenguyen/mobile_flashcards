7947404d12596c6d9f4334a8615ee5ad886d8c49
Alize Nguyen <alizenguyen@gmail.com>
2018-09-05 13:43:58 -0500

    api added

---
+++ utils/api.js
@@ -0,0 +1,43 @@
+import { AsyncStorage } from 'react-native'
+
+export function getDecks () {
+  return AsyncStorage.getAllKeys((err, keys) => {
+    AsyncStorage.multiGet(keys, (err, stores) => {
+      stores.map((result, i, store) => {
+        // get at each store's key/value so you can work with it
+        let key = store[i][0];
+        let value = store[i][1];
+
+        if (value) {
+          return {
+            key,
+            title: value.title,
+            questions: value.questions
+          };
+        }
+      });
+    });
+  });
+}
+
+export function getDeck (key) {
+  return AsyncStorage.getItem(key);
+}
+
+export function saveDeckTitle (title) {
+  return AsyncStorage.setItem(title, JSON.stringify({ title, questions: [] }));
+}
+
+export function addCardToDeck (title, card) {
+  AsyncStorage.getItem(title).then(deck => {
+    const data = JSON.parse(deck);
+    let questions = data.questions;
+
+    questions.push(card);
+
+    //merges the updated deck with the old one
+    AsyncStorage.mergeItem(title, JSON.stringify({
+      questions
+    }));
+  });
+}
