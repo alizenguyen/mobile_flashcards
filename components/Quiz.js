5afe7311736c73444d05f40669dc8acb3f838896
Alize Nguyen <alizenguyen@gmail.com>
2018-09-06 11:55:22 -0500

    quiz ui added

--- components/Quiz.js
+++ components/Quiz.js
@@ -23,23 +23,37 @@ class Quiz extends Component {
       <View>
         <Text>
           {currentQuestion < questions.length
-          ? [(showQuestion === false
-              ? <View>
-                  <Text>{questions[currentQuestion].question}</Text>
-                  <TouchableOpacity>
-                    <Text>
-                      View Answer
-                    </Text>
-                  </TouchableOpacity>
-                </View>
-              : <View>
-                  <Text>{questions[currentQuestion].answer}</Text>
-                  <TouchableOpacity>
-                    <Text>
-                      Return to Question
-                    </Text>
-                  </TouchableOpacity>
-                </View>)]
+          ? <View>
+              [(showQuestion === false
+                  ? <View>
+                      <Text>{questions[currentQuestion].question}</Text>
+                      <TouchableOpacity>
+                        <Text>
+                          View Answer
+                        </Text>
+                      </TouchableOpacity>
+                    </View>
+                  : <View>
+                      <Text>{questions[currentQuestion].answer}</Text>
+                      <TouchableOpacity>
+                        <Text>
+                          Return to Question
+                        </Text>
+                      </TouchableOpacity>
+                    </View>)]
+                {/* Correct Answer Button */}
+                <TouchableOpacity>
+                  <Text>
+                    Correct
+                  </Text>
+                </TouchableOpacity>
+                {/* Correct Answer Button */}
+                <TouchableOpacity>
+                  <Text>
+                    Incorrect
+                  </Text>
+                </TouchableOpacity>
+              </View>
           : <Text>No more questions.</Text>
           }
         </Text>
