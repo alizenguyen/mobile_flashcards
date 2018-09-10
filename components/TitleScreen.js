48b733d52ae4eb8e8347f845db3f1f27953f40ee
Alize Nguyen <alizenguyen@gmail.com>
2018-09-10 16:56:22 -0500

    background image added (not working)

--- components/TitleScreen.js
+++ components/TitleScreen.js
@@ -1,14 +1,30 @@
 import React from 'react'
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, ImageBackground } from 'react-native';
 import { MaterialCommunityIcons } from '@expo/vector-icons'
 
 export default function TitleScreen () {
+
   return (
     <View>
-      <MaterialCommunityIcons
-        name={'cards-outline'}
-        size={100}
-      />
+      <ImageBackground 
+        source={require('../assets/images/title_background.png')}
+        style={styles.backgroundImage}
+        imageStyle={{resizeMode: 'cover'}}
+      >
+        <Text>test</Text>
+        {/* <MaterialCommunityIcons
+          name={'cards-outline'}
+          size={100}
+        /> */}
+      </ImageBackground>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  backgroundImage: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+  }
+});
